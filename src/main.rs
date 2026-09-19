646006616331cad8cf56082e23c1810791757b0f
rohitJung <rokshh28@gmail.com>
2026-09-19 21:39:44 +0545

    refactor: separate lookup fns

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,12 @@
 use std::net::UdpSocket;
 
 use dns_resolver::{
-    protocol::{packet::DnsPacket, question::DnsQuestion},
+    protocol::{lookup::recursive_lookup, packet::DnsPacket},
     types::{
         bytepacket_buffer::{BytePacketBuffer, Result},
-        query_type::QueryType,
         result_code::ResultCode,
     },
 };
-
-fn lookup(qname: &str, qtype: QueryType) -> Result<DnsPacket> {
-    // use cloudflare dns
-    let cloudflare_dns = ("1.1.1.1", 53);
-    let socket = UdpSocket::bind(("0.0.0.0", 43210))?;
-
-    let mut packet = DnsPacket::new();
-    packet.header.id = 6969;
-    packet.header.question_count = 1;
-    packet.header.recursion_desired = true;
-    packet
-        .questions
-        .push(DnsQuestion::new(qname.to_string(), qtype));
-
-    // let mut f = File::open("raw_packets/response_packet.txt")?;
-    let mut req_buf = BytePacketBuffer::new();
-
-    // write the packet into req_buf  and send to server using socket
-    packet.write(&mut req_buf)?;
-    socket.send_to(&req_buf.buf[0..req_buf.pos], cloudflare_dns)?;
-
-    // socket write response to res_buf
-    let mut res_buf = BytePacketBuffer::new();
-    socket.recv_from(&mut res_buf.buf)?;
-
-    // parse the recevied response
-    DnsPacket::from_buffer(&mut res_buf)
-}
 
 fn handle_query(socket: &UdpSocket) -> Result<()> {
     let mut req_buf = BytePacketBuffer::new();
@@ -57,7 +28,7 @@ fn handle_query(socket: &UdpSocket) -> Result<()> {
 
         // forward the query to the target server
         // if anything goes wrong SERVFAIL, else records are copied in response packet
-        if let Ok(result) = lookup(&question.name, question.qtype) {
+        if let Ok(result) = recursive_lookup(&question.name, question.qtype) {
             packet.questions.push(question);
 
             for a in result.answers {
@@ -86,6 +57,7 @@ fn handle_query(socket: &UdpSocket) -> Result<()> {
     packet.write(&mut res_buf)?;
 
     let len = res_buf.pos();
+    println!("response buffer to write {:?}", packet);
     let data = res_buf.get_range(0, len)?;
 
     socket.send_to(data, src)?;
